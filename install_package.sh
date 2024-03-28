c039f9d3a043cf2204389adb1b5478ba876ecf1f
Vitor Augusto <131685750+vitorAugusto2@users.noreply.github.com>
2024-03-28 14:34:19 -0300

    Add files via upload

---
+++ install_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#!pip install -q \
+#autodistill \
+#autodistill-grounded-sam \
+#autodistill-yolov8 \
+#supervision==0.9.0 \
+#roboflow
+
+pip install autodistill
+pip install autodistill autodistill-grounded-sam autodistill-yolov8
+pip install roboflow
